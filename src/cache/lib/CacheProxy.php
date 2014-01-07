a7a736a55918042ca8848d4df4953105e61002c5
Piotr Miazga <raynor2k@gmail.com>
2014-01-07 00:28:47 -0600

    Added presentation, fixed CacheProxy

--- src/cache/lib/CacheProxy.php
+++ src/cache/lib/CacheProxy.php
@@ -53,3 +53,14 @@ class CacheWrapper extends ObjectWrapper {
         return $this->_cache->delete($this->_createKey($method, $arguments));
     }
 }
+
+
+class SessionHandler implements SessionHandlerInterface {
+
+    public bool close()
+    public bool destroy( string $session_id )
+    public bool gc( int $maxlifetime )
+    public bool open( string $save_path , string $session_id )
+    public strin gread( string $session_id )
+    public bool write( string $session_id , string $session_data )
+}
